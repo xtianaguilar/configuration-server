1d7050d0172ad79ed56b8c3ca7e1644a128b30ec
Christian Aguilar <xtian.aguilar@gmail.com>
2018-12-05 20:32:31 -0600

    git config

--- configuration-server/src/main/java/com/configuration/server/ConfigurationServerApplication.java
+++ configuration-server/src/main/java/com/configuration/server/ConfigurationServerApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @EnableConfigServer
+@RefreshScope
 public class ConfigurationServerApplication {
 
 	public static void main(String[] args) {
